25ba6adfcb2a809b4c8603e6780f87ef62084c6b
Stopresent <86364202+stopresent@users.noreply.github.com>
2023-09-04 16:53:24 +0900

    [ADD] 기본 스크립트 추가 (-현재)
    기본 스크립트 추가

---
+++ Assets/@Scripts/Managers/Contents/GameManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using static Define;
+
+[Serializable]
+public class GameData
+{
+    
+}
+
+public class GameManager
+{
+
+
+}
---
+++ Assets/@Scripts/Utils/Define.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Utils;
+
+public class Define
+{
+   
+    #region Enum
+
+    public enum Scene
+    {
+        Unknown,
+        TitleScene,
+        LobbyScene,
+        GameScene,
+    }
+
+    public enum Sound
+    {
+        Bgm,
+        SubBgm,
+        Effect,
+        Max,
+    }
+
+    public enum UIEvent
+    {
+        Click,
+        Preseed,
+        PointerDown,
+        PointerUp,
+        BeginDrag,
+        Drag,
+        EndDrag,
+    }
+    #endregion
+
+}
